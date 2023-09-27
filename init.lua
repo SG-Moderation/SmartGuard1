bda75299e8662cc1622f7efaea9d1f64d660e586
GreenBlob <lightboxfacts@gmail.com>
2023-09-27 18:16:39 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -51,7 +51,7 @@ end
 
 -- messages without a space and contain 20 or more characters will trigger this
 function automod.contains_spam(target_message)
-    if string.len(target_message) > 20
+    if string.len(target_message) > 15
     and not string.find(target_message, ' ') then
         return true
     else
@@ -65,32 +65,24 @@ local blacklist_simple = {"fu", "sht"}
 
 minetest.register_on_chat_message(function(name, message)
     if automod.contains_pattern(message, blacklist_complex) then
-        minetest.chat_send_all("No swearing please!")
-        return true
+        minetest.chat_send_all("Swearing detected.")
     end
-    return false
 end)
 
 minetest.register_on_chat_message(function(name, message)
     if automod.contains_word(message, blacklist_simple) then
-        minetest.chat_send_all("No swearing please!")
-        return true
+        minetest.chat_send_all("Swearing detected.")
     end
-    return false
 end)
 
 minetest.register_on_chat_message(function(name, message)
     if automod.contains_caps(message) then
-        minetest.chat_send_all("No all caps please!")
-        return true
+        minetest.chat_send_all("All-caps detected.")
     end
-    return false
 end)
 
 minetest.register_on_chat_message(function(name, message)
     if automod.contains_spam(message) then
-        minetest.chat_send_all("No spamming please!")
-        return true
+        minetest.chat_send_all("Spamming detected.")
     end
-    return false
 end)
