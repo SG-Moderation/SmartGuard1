891fac304f7c11f51da1d6b089cee1821fad28b3
GreenBlob <lightboxfacts@gmail.com>
2023-09-28 08:27:22 +0800

    Update anti-bypass.lua

--- anti-bypass.lua
+++ anti-bypass.lua
@@ -1,22 +1,23 @@
-local last_three_messages = {}
-local blacklist_bypass = {"fuc", "fuk", "sht", "fck"}
+local last_messages = {}
+local blacklist_bypass = {"fuck", "fu", "shit", "fck", "sht", "hello"}
 
 minetest.register_on_chat_message(function(name, message)
 
-    if not last_three_messages[name] then
-        last_three_messages[name] = {}
+    if not last_messages[name] then
+        last_messages[name] = {}
     end
 
-    table.insert(last_three_messages[name], message)
+    table.insert(last_messages[name], message)
 
-    if #last_three_messages[name] > 3 then
-        table.remove(last_three_messages[name], 1)
+    if #last_messages[name] > 20 then
+        table.remove(last_messages[name], 1)
     end
 
-    local last_three_messages_str = table.concat(last_three_messages[name])
+    local last_messages_str = table.concat(last_messages[name])
     for _, word in ipairs(blacklist_bypass) do
-        if last_three_messages_str == word then
+        if string.find(last_messages_str, word) then
             minetest.chat_send_all("Swearing bypass detected.")
+            last_messages[name] = {}
             break
         end
     end
