cb763e8866f2811cfc999808f5c5258730eb4193
GreenBlob <lightboxfacts@gmail.com>
2023-10-10 15:13:15 +0800

    Add a note

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
 dofile(minetest.get_modpath(minetest.get_current_modname()) .. "/functions.lua")
 
+--[[
+    remember that Lua’s string matching functions like string.find use patterns
+    similar to regular expressions, which means certain characters like ., %, *, -, etc.
+    have special meanings. If your blacklist contains any of these characters
+    and you want to match them literally, you’ll need to escape them using the % character.
+]]
+
 local blacklist_smart = {"fuck", "shit", "bitch", "fu ", "f u ", "f u c k", "s h i t", "helo", "h e l l o"}
 local blacklist_simple = {"fu", "sht"}
 
