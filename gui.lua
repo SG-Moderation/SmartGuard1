bdbc5cde9490eaa5446d05f74855492839e5b9ee
GreenBlob <lightboxfacts@gmail.com>
2023-10-27 23:50:12 +0800

    Fix Crash

--- gui.lua
+++ gui.lua
@@ -2,8 +2,11 @@ sfinv.register_page("automod:gui", {
     title = "AutoMod",
     get = function(self, player, context)
         local file = io.open(minetest.get_worldpath() .. "/AUTOMOD_LOGS.txt")
-        local content = file:read("*all")
-        file:close()
+        local content = ""
+        if file then
+            content = file:read("*all")
+            file:close()
+        end
         local gui_elements = "textarea[0.3,0;8,10;fieldlogs;;" .. content .. "]" ..
                              "button[0,8.5;4,1;refresh;Refresh]" ..
                              "button[4,8.5;4,1;clear_field;Clear]"
